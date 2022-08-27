25def6518be64f6513d29b907143c40cc09ce209
PiyushWadhawan <66672056+PiyushWadhawan@users.noreply.github.com>
2022-08-27 13:48:49 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ function Header({theme, darkmode}) {
     <header>
       <div className='container container-flex'>
         <div className='site-title'>
-            <h1>Blog Station</h1>
+            <h1>Blogs Station</h1>
             <p className='subtitle'>Write to your heart's content</p>
         </div>
         <Navbar theme={theme} darkmode={darkmode}/>
